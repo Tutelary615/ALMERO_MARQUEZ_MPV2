a4c98251f1a737f6d4bdd2efa08a11aa09f0e783
Tutelary615 <marqueztj34@gmail.com>
2025-03-15 14:13:51 +0800

    added old functions

--- MP_V2.c
+++ MP_V2.c
@@ -1,5 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "MP_HEADER.h"
 
+void 
+initString(char str[])
+{
+    int i;
+    for (i = 0; i < strlen(str); i++)
+    {
+        str[i] = '\0';
+    }
+}
+
+bool 
+getInteger(int* num)
+{
+    char firstCharacterEntered;
+    char lastCharacterAfterNum;
+    bool isInputValid = false;
+
+    firstCharacterEntered = getc(stdin);
+
+    if (firstCharacterEntered != '\n')
+    {
+        ungetc(firstCharacterEntered, stdin);
+        scanf("%d", num);
+        lastCharacterAfterNum = getc(stdin);
+    }
+
+    if (lastCharacterAfterNum == '\n')
+    {
+        isInputValid = true;
+    }
+    fflush(stdin);
+    return isInputValid;
+}
+
+bool
+isMenuInputValid(bool isInputValid, int lowerBound, int upperBound, int input)
+{
+	bool isValid = true;
+
+	if (!isInputValid || input < lowerBound || input > upperBound)
+	{
+		printf(REDFORMATSTRING, "Entered number is invalid, try again\n");
+		isValid = false;
+	}
+		
+	return isValid;
+}
+
+int 
+getAndValidateMenuInput(int lowerBound, int upperBound)
+{
+    int choice;
+    bool isChoiceValid;
+    do
+	{
+        printf("Enter choice: ");
+		isChoiceValid = getInteger(&choice);
+				
+	} while (!isMenuInputValid(isChoiceValid, 1, upperBound, choice));
+	return choice;
+}
+
+void 
+printMenu(string50 choices[], int numberOfChoices)
+{
+    int i;
+    for (i = 0; i < numberOfChoices; i++)
+    {
+        printf("%d - %s\n", (i + 1), choices[i]);
+    }
+}
+
+int
+mainMenu()
+{
+	int choice;
+	bool isChoiceValid;
+	string50 options[3] = {"Manage Data", "Translate Menu", "Exit"};
+	
+	printMenu(options, 3);
+	
+	do
+	{
+		printf("Select an option from the menu above: ");
+		isChoiceValid = getInteger(&choice);
+				
+	} while (!isMenuInputValid(isChoiceValid, 1, 3, choice));
+	return choice;
+}
+
+int 
+manageDataMenu(int entryCount)
+{
+    string50 options[10] = {"Add Entry", "Import Data", "Add Translations", "Delete Entry", "Delete Translation",
+                            "Display All Entries", "Search by Word", "Search By Translation", "Export Data", "Exit"};
+    int choice;
+    int upperBound;
+    if (entryCount > 0)
+    {
+        printMenu(options, 10);
+        upperBound = 10;
+    }
+    else
+    {
+        printMenu(options, 2);
+        upperBound = 2;
+    }
+    printf("Select and option from the menu above\n");
+    choice = getAndValidateMenuInput(1, upperBound);
+    return choice;
+}
