b773a1948695d6ddc450c15d384b386bca0455cc
Tutelary615 <marqueztj34@gmail.com>
2025-03-25 14:03:28 +0800

    defined HIGHLIGHTED_PAIR_FORMAT_STRING

--- MP_HEADER.h
+++ MP_HEADER.h
@@ -2,6 +2,7 @@
 #define YELLOWFORMATSTRING "\033[0;33m%s\033[0m"
 #define GREENFORMATSTRING "\033[0;32m%s\033[0m"
 #define PURPLEFORMATSTRING "\033[0;35m%s\033[0m"
+#define HIGHLIGHTED_PAIR_FORMAT_STRING "\033[0;36m%s: %s <\033[0m\n"
 #define MAX_ENTRIES 150
 #define MAX_PAIRS_PER_ENTRY 10
 
@@ -18,3 +19,8 @@ typedef struct {
 	LTPairType pairs[MAX_PAIRS_PER_ENTRY];
 	int pairCount;
 } entryType;
+
+typedef struct {
+    int entryIndex;
+    int pairIndex;
+} LTPairIndex;
