cb57284ea552d0868467db71956ec3930ca27bcf
Antonio Alvaro Almero <oinotnaoremla@gmail.com>
2025-03-30 21:56:14 +0800

    tweaks

--- MP_V2.c
+++ MP_V2.c
@@ -80,6 +80,7 @@ getInteger(int* num)
    @param charSet - string consisting of valid characters
    @return true if the character bebing checked is among the set of characters
            and false otherwise
+	Pre-condition: Input is a character
 */
 bool 
 isCharInSet(char charToCheck, char charSet[])
@@ -400,6 +401,7 @@ isOperationConfirmed()
 /* formatLanguage formats a string such that it begins with a capital letter,
    and all proceeding letters are lowercase and contains no newline character
    @param language - string to be formatted
+   Pre-condition: language string is valid
 */
 void 
 formatLanguage(string20 language)
@@ -422,6 +424,7 @@ formatLanguage(string20 language)
 /* formatTranslation formats a string such that all letters are lowercase and
    contains no newline character
    @param translation - string to be formatted
+   Pre-condition: translation string is valid
 */
 void 
 formatTranslation(string20 translation)
@@ -749,7 +752,7 @@ initEntry(entryType entries[], int* entryCount)
 			matchCount++;
 		}			
 	}
-	
+
 	if (matchCount > 0)
 	{
 		printf("%d entry/entries with matching translation found.\n", matchCount);	
@@ -1009,7 +1012,8 @@ removeLTPair(entryType* entry, int indexOfPairToRemove)
    Pre-condition: the value stored in entryCount is more than 0
 */
 
-void removeEntry(entryType entries[], int delIndex, int* entryCount)
+void 
+removeEntry(entryType entries[], int delIndex, int* entryCount)
 {
     int i;
     for (i = delIndex; i < *entryCount - 1; i++)
@@ -1975,7 +1979,7 @@ findKeyEntry(entryType sourceEntries[], int sourceEntriesCount, LTPairType keyPa
 	
 	for (i = 0; i < sourceEntriesCount && !found; i++)
     {
-        if (searchForLTPair(sourceEntries[i], keyPair.language, keyPair.translation) != -1);
+        if (searchForLTPair(sourceEntries[i], keyPair.language, keyPair.translation) != -1)
         {
             index = i;
             found = true;
@@ -2272,7 +2276,6 @@ translateFile(entryType sourceEntries[], int sourceEntriesCount)
 		printf("Would you like to translate another text file?\n");			
 		
 	} while (isOperationConfirmed());
-	
 	printf("\n");
 }
 
